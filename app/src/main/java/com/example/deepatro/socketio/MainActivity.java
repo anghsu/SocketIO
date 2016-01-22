b8022f93b01f9495ef2a15664e05421a12a9eddd
anghsu <anghsu@cisco.com>
2016-01-21 20:57:38 -0800

    PSEUDOCODE PRINT LOG

--- app/src/main/java/com/example/deepatro/socketio/MainActivity.java
+++ app/src/main/java/com/example/deepatro/socketio/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.deepatro.socketio;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -11,6 +12,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -40,8 +46,24 @@ public class MainActivity extends ActionBarActivity {
                 Log.d("clicked view", "");
                 setContentView(R.layout.activity_log);
                 logdump = (TextView) findViewById(R.id.logdump);
+                StringBuilder text = new StringBuilder();
+                try {
+                    String dirpath = "DIRECTORY PATH";
+                    String filename = "FILENAME";
+                    File file = new File(dirpath,filename);
+
+                    BufferedReader br = new BufferedReader(new FileReader(file));
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        text.append(line);
+                        text.append('\n');
+                    }
+                    br.close() ;
+                }catch (IOException e) {
+                    return;
+                }
                 //store logdump to be dump of file
-                logdump.setText("BLAHBALHBLAHBHLAHBAL");
+                logdump.setText(text);
             }
         });
     }
