6c1746fd81bd707471b71ee8b057f2c2a847ea99
xsdf <gregginsmcgee@gmail.com>
2016-01-21 17:28:12 -0800

    create notifications on recieving data

--- app/src/main/java/com/example/deepatro/socketio/SocketListenerService.java
+++ app/src/main/java/com/example/deepatro/socketio/SocketListenerService.java
@@ -1,8 +1,13 @@
 package com.example.deepatro.socketio;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 import com.github.nkzawa.emitter.Emitter;
@@ -18,10 +23,16 @@ public class SocketListenerService extends Service {
     public SocketListenerService() {
     }
 
+    private int notifyId;
+    private String user;
+    private String laas_server;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
+        notifyId = 1;
+        user ="greggmi";
+        laas_server = "laas-greggmi";
+        startSocket();
         Log.d("socket instance", "connecting");
         socket.connect();
         socket.on("reservations", notifyIncomingMessages);
@@ -30,9 +41,10 @@ public class SocketListenerService extends Service {
     }
 
     private Socket socket;
-    {
+
+    private void startSocket(){
         try {
-            socket = IO.socket("http://laas-greggmi.cisco.com:8000");
+            socket = IO.socket("http://" + laas_server + ".cisco.com:8000");
             Log.d("socket instance", "created");
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
@@ -44,18 +56,58 @@ public class SocketListenerService extends Service {
         public  void  call(final Object... args){
 
                 JSONObject data = (JSONObject) args[0];
-                Log.i("json is", data.toString());
-                String message;
+                Log.w("json is", data.toString());
+                String event, user_name, topo_name, message;
                 try {
-                    message = data.getString("message");
-
+                    event = data.getString("event");
+                    user_name = data.getString("user_name");
+                    topo_name = data.getString("topo_name");
                 } catch (JSONException e) {
+                    Log.e("Value not found", e.getMessage());
                     return;
                 }
-                Log.d("message is", message);
-
+                if (user.equals(user_name)) {
+                    message = "Your topology, " + topo_name + ", has been " + event;
+                    sendNotification(android.R.drawable.ic_menu_view, "LaasNG", message);
+                }else{
+                    Log.e("username not matching"+user, data.toString());
+                }
         }
     };
+
+
+    private void sendNotification(int icon, CharSequence title,  CharSequence notifyText){
+        android.support.v4.app.NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(getApplicationContext())
+                        .setSmallIcon(android.R.drawable.ic_menu_view)
+                        .setContentTitle(title)
+                        .setContentText(notifyText);
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(notifyId ,mBuilder.build());
+        notifyId ++;
+
+    }
+
 
     @Override
     public void onDestroy() {
